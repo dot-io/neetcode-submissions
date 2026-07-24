fe88799ff4e75d630e32f393c45def0ed6f50521
Nathan M. <70782371+dot-io@users.noreply.github.com>
2026-07-24 08:49:19 +0200

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        letter_hash = {}
+        for string in strs:
+            hash_val = [0] * 26
+            for c in string:
+                hash_val[ord(c)-ord('a')] += 1
+                print(hash_val)
+            key = "".join([chr(e) for e in hash_val]) #str causes problems with double-digit freqs
+            print(key)
+            if not key in letter_hash.keys():
+                letter_hash[key] = []
+            letter_hash[key].append(string)
+        return list(letter_hash.values())
+
+
+        
