924c9fe61de7c1891f72d6abaa08471eb3f1104a
Nathan M. <70782371+dot-io@users.noreply.github.com>
2026-07-28 21:40:15 +0200

    Add: sql-calculate-special-bonus - submission-0

---
+++ SQL/sql-calculate-special-bonus/submission-0.sql
@@ -0,0 +1,3 @@
+-- Write your query below
+SELECT employee_id, CASE WHEN (employee_id % 2 = 1) AND (SUBSTRING(name from 1 for 1) != 'M') THEN (salary) ELSE 0 END AS bonus
+FROM employees ORDER BY employee_id;
